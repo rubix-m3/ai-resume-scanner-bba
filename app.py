8d047597240ccde716578a94e62e79353305237f
rubix-m3 <sachinsourav9333@gmail.com>
2026-01-09 00:04:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -270,4 +270,10 @@ def rank():
 # ================= RUN =================
 if __name__ == "__main__":
     print("✅ Resume Scanner FINAL VERSION running...")
-    app.run(debug=True)
+    import os
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
+
+
